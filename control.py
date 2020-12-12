487d6b4d6ccdc4e44eb60e84af15eb8337090295
Rafael-Benicio <jerobenicio@gmail.com>
2020-12-12 20:00:14 -0300

    Atualização

--- control.py
+++ control.py
@@ -1,116 +1,54 @@
 import PySimpleGUI as sg
-from time import sleep
 
-nuns=''
-Pau=False
-SleepTime=0
-Hou=0
-Min=0
-Sec=0
-event=''
+sg.theme("Dark Amber")
 
-layout=[[sg.Button('Tocar '),sg.Button('Pausar'),sg.Button('Parar '),sg.Text('',size=(7,1),key='val',font="Helvetica "  + str(20))]]
+def init():
+    num=[]
+    tex=''
+    Gat=True
 
-sg.theme('Dark Purple')
+    lay=[[sg.Button("Play"),sg.Button("Limpar"),sg.Text(tex,font=(16,16),size=(8,1),key="-nun-")]]
 
-window=sg.Window('Crônos',layout, no_titlebar=True, auto_size_buttons=False, keep_on_top=True, grab_anywhere=True, return_keyboard_events=True,use_default_focus=True)
+    window=sg.Window('PyCron',lay, return_keyboard_events=True)
 
+    while Gat:
+        e, v =window.read()
+        window["-nun-"].update(tex)
+        if e== sg.WINDOW_CLOSED:
+            exit()
+        if e=='0' or e=='1' or e=='2' or e=='3' or e=='4' or e=='5' or e=='6' or e=='7' or e=='8' or e=='9':
+            print(len(tex))
+            if len(tex)<=7:
+                if len(tex)==2 or len(tex)==5:
+                    tex=':'+tex
+                tex=e+tex
 
-# Para receber o valor passado no timer   
-def Recebe():
-    global nuns
-    if len(nuns)<=7  and event == '1' or len(nuns)<=7  and event == '2' or len(nuns)<=7  and event == '3' or len(nuns)<=7  and event == '4' or len(nuns)<=7  and event == '5' or len(nuns)<=7  and event == '6' or len(nuns)<=7  and event == '7' or len(nuns)<=7  and event == '8' or len(nuns)<=7  and event == '9' or len(nuns)<=7  and event == '0':
-        nuns=nuns+event
-        if len(nuns)==2 or len(nuns)==5:
-            nuns=nuns+':'
-        window['val'].update(nuns)
+                n=[int(e)]
+                n.extend(num)
+                num=n
+        if e=="Limpar":
+            tex=''
+            num=[]
+        if e=="Play" and num!=[]:
+            Toca(num)
+            break
+        window["-nun-"].update(tex)
 
-# Transformação de str para int 
-def Inicia():
-    global Pau
-    Pau=True
-    try:
-        Hou=int(nuns[0]+nuns[1])
-    except:
-        Hou=00
-    try:
-        Min=int(nuns[3]+nuns[4])
-    except:
-        Min=00
-    try:
-        Sec=int(nuns[6]+nuns[7])   
-    except:
-        Sec=00
-    print(Hou, Min, Sec)
+    window.close()
 
-# Atalhos de Teclado
-def Snipper():
-    global event
-    if event == 'F12:96':
-        window.close()
-    if event == 'F1:67' or event == 'Tocar ':
-        if Pau==False:
-            Inicia()
-        event=''
-    if event == 'F2:68' or event == 'Pausar':
-        Pausar()
-        event=''
-    if event == 'F3:69' or event == 'Parar ':
-        Parar()
-        event=''
+def Toca(n):
+    print(n)
+    lay2=[[sg.Button("Pausar"),sg.Button("Parar"),sg.Text(font=(16,16),size=(8,1),key="-nu-")]]
 
-# Apaga o ultimo valor da timer
-def ApagaLetra():
-    global nuns
-    newNun=''
-    key=len(nuns)-1
-    if event == ']':
-        for i in range(key):
-            newNun+=nuns[i]
-        nuns=newNun
-        window['val'].update(nuns)
-        print(nuns)
+    window2=sg.Window("Toncando",lay2)
 
-# Pausar ou Rodar
-def Pausar():
-    global Pau
-    if Pau==False:
-        Pau=True
-    else:
-        Pau=False
-    print(Pau)
+    while True:
+        e, v=window2.read()
+        if e== sg.WINDOW_CLOSED or e=="Parar":
+            init()
+            break
+        window["-nun-"].update(1)
 
-# Para o processo
-def Parar():
-    global Pau 
-    Pau=False
-    print(Pau)
+    window2.close()
 
-# Timer
-def Tocar():
-    global Sec 
-    global Min
-    global Hou
-    global event
-    global SleepTime
-    sleep(1)
-    SleepTime+=1
-    
-        # if Sec == 0 and Min == 0 and Hou == 0:
-                # Finaliza e toca musica
-    print(SleepTime)
-    
-# Loop Principal
-while True:
-    event,value=window.read(100)
-    if event == sg.WIN_CLOSED:
-        break
-    Snipper()
-    Recebe()
-    ApagaLetra()
-    if Pau:Tocar()
-    if Pau:
-        window['val'].update(f'{Hou}:{Min}:{Sec}')
-
-
-window.close()
+init()
