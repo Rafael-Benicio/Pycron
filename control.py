1039054a7d7898d8a81398f8a33ca696b5891af8
Rafael-Benicio <jerobenicio@gmail.com>
2020-12-13 18:59:20 -0300

    Tempo

--- control.py
+++ control.py
@@ -1,6 +1,10 @@
 import PySimpleGUI as sg
+from time import sleep
 
 sg.theme("Dark Amber")
+
+tic=0
+timerGeral=''
 
 def init():
     num=[]
@@ -30,25 +34,84 @@ def init():
             tex=''
             num=[]
         if e=="Play" and num!=[]:
-            Toca(num)
             break
         window["-nun-"].update(tex)
 
     window.close()
+    Toca(num)
 
 def Toca(n):
+    
+    timer=100
+    boolCont=True
+    valor='00:00:00'
+    seg=0
+    minu=0
+    hor=0
+    Segtmp=''
+    Mintmp='00'
+    Hortmp='00'
     print(n)
     lay2=[[sg.Button("Pausar"),sg.Button("Parar"),sg.Text(font=(16,16),size=(8,1),key="-nu-")]]
 
     window2=sg.Window("Toncando",lay2)
 
     while True:
-        e, v=window2.read()
+        global timerGeral
+        global tic
+        tic+=1
+        # Velocidade do while
+        if boolCont:
+            timer=100
+        else:
+            timer=None
+
+        e, v=window2.read(timer)
+
         if e== sg.WINDOW_CLOSED or e=="Parar":
-            init()
+            timerGeral=''
             break
-        window["-nun-"].update(1)
+        if e=="Pausar":
+            # Pausar o contador
+            if boolCont:
+                boolCont=False
+            else:
+                boolCont=True
+        
+        if tic==10:
+            # Segundo contador
+            seg+=1
+            Segtmp=str(seg)
+
+            if seg==60:
+                seg=0
+                minu+=1
+                if minu==60:
+                    minu=0
+                    hor+=1
+                    # Hora contador
+                    Hortmp=str(hor)
+                    if hor<10:
+                        Hortmp='0'+str(hor)
+                # Minutos contador
+                Mintmp=str(minu)
+                if minu<10:
+                    Mintmp='0'+str(minu)
+
+
+            if seg<10:
+                segn='0'+str(seg)
+                Segtmp=segn
+
+            timerGeral=Hortmp+':'+Mintmp+':'+Segtmp
+            tic=0
+        
+        # Atualização do texto do timer
+        window2["-nu-"].update(timerGeral)
 
     window2.close()
+    init()
 
 init()
+
+
